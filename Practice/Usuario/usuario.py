4d66ac5140517b399ab7315679bfbfbad300b3df
NicoDS9688 <nicolasdickel@hotmail.com>
2023-07-18 14:38:11 -0400

    encadenar metodos

--- Practice/Usuario/usuario.py
+++ Practice/Usuario/usuario.py
@@ -6,12 +6,15 @@ class Usuario:
 
     def hacer_deposito(self, amount):
         self.balance_cuenta += amount
+        return self
 
     def hacer_retiro(self, amount):
         self.balance_cuenta -= amount
+        return self
 
     def mostrar_balance(self):
         print(self.balance_cuenta)
+        return self
 
     def hacer_transferencia(self, amount, receiver):
         if self.balance_cuenta >= amount:
@@ -20,8 +23,7 @@ class Usuario:
             print(f"{self.name} ha tranferido ${amount} a {receiver.name}")
         else:
             print("Fondos insuficientes para realizar transferencia")
-
-
+        return self
 
 
 bobby = Usuario("Bobby Lee", "bobbylee@python.com")
@@ -30,27 +32,14 @@ john = Usuario("John Shepard", "cmdrshep@alliance.com")
 
 geralt = Usuario("Geralt de Rivia", "geraltwitcher@gmail.com")
 
-bobby.hacer_deposito(250)
-bobby.hacer_deposito(550)
-bobby.hacer_deposito(755)
-bobby.hacer_retiro(500)
-bobby.mostrar_balance()
+bobby.hacer_deposito(25).hacer_deposito(550).hacer_deposito(755).hacer_retiro(500).mostrar_balance()
 
-john.hacer_deposito(200)
-john.hacer_deposito(150)
-john.hacer_retiro(150)
-john.hacer_retiro(350)
-john.mostrar_balance()
+john.hacer_deposito(200).hacer_deposito(150).hacer_retiro(150).hacer_retiro(350).mostrar_balance()
 
-geralt.hacer_deposito(1000)
-geralt.hacer_retiro(333)
-geralt.hacer_retiro(333)
-geralt.hacer_retiro(333)
-geralt.mostrar_balance()
+geralt.hacer_deposito(1000).hacer_retiro(333).hacer_retiro(333).hacer_retiro(333).mostrar_balance()
 
 
-bobby.hacer_transferencia(200, geralt)
-bobby.mostrar_balance()
+bobby.hacer_transferencia(200, geralt).mostrar_balance()
 geralt.mostrar_balance()
 
 
